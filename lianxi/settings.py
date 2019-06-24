e580420572be4b153815a6b656166b2d83311ec7
wb3344 <1421269795@qq.com>
2019-06-25 00:36:17 +0800

    43234243243

--- lianxi/settings.py
+++ lianxi/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'lianxi.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': 3306,
     }
 }
 
